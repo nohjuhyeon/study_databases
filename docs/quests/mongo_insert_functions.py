7a058e1d795dd2cd1c1d8d85641cc7627befa9a8
nohjuhyeon <njh2720@gmail.com>
2023-12-14 10:27:56 +0900

    수정

--- docs/quests/mongo_insert_functions.py
+++ docs/quests/mongo_insert_functions.py
@@ -1,23 +1,30 @@
 from pymongo import MongoClient
-def connect():
-    mongoClient = MongoClient("mongodb://localhost:27017")
-    database = mongoClient["local"]
-    collection = database['fruits_info']
+
+def connect(mongo_server_link,database_name,collection_name):                # collection에 연결하는 함수 작성
+    mongoClient = MongoClient(mongo_server_link)                             # mongo DB 서버에 연결
+    database = mongoClient[database_name]                                    # 데이터 베이스에 연결
+    collection = database[collection_name]                                   # collection에 연결
     return collection
 
-def insert(collection,dict_fruit):
-    collection.insert_one(dict_fruit)
+def insert(collection,dict_fruit):                                      # 리스트를 추가하는 함수 작성
+    collection.insert_one(dict_fruit)                                   
     return
 
-fruit_info = [
+def insert_list(list_items):                                                    #리스트의 dict들을 모두 추가하는 함수 작성
+    collection = connect(mongo_server_link,database_name,collection_name)       # 추가하고자 하는 collection에 연결
+    for i in range(len(list_items)):                                            # 리스트의 dict들이 모두 추가될 때까지 반복
+        dict_fruit = list_items[i]                                              
+        insert(collection,dict_fruit)                                           # 리스트의 dict 추가
+
+fruit_info = [                                                         #추가할 리스트 작성
     {"name": "사과", "color": "빨강", "taste": "달콤"},
     {"name": "바나나", "color": "노랑", "taste": "달콤"},
     {"name": "포도", "color": "보라", "taste": "달콤한데 약간의 쓴맛이 있음"},
     {"name": "오렌지", "color": "주황", "taste": "달콤하면서 새콤"}
 ]
 
-for i in range(len(fruit_info)):
-    dict_fruit = fruit_info[i]
-    collection = connect()
-    insert(collection,dict_fruit)
+mongo_server_link = "mongodb://localhost:27017"                                 # mongoDB 서버 입력
+database_name ="local"                                                          # 데이터 베이스 이름 입력
+collection_name = 'fruits_info'                                                 # collection 이름 입력
 
+insert_list(fruit_info)
